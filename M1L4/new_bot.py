405fada6d59d92e29938c68e018fb8a54c985939
NPC1234567890 <julia.lozada@.com>
2023-10-27 19:34:33 -0300

    primer compromiso

---
+++ M1L4/new_bot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+bot.run('MTE1OTk3MDEzNjU3NzU0MDE5Nw.GujnpY.HSSiK2-Ovyt2kf8_re_WixR2BT5XPRwwJKkfLQ')
